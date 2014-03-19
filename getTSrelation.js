324a390502e18c7fbbfa3bb363713a1c14ad7a58
timmy00274672 <timmy00274672@gmail.com>
2014-03-19 22:24:17 +0800

    [Success]Test for binding db.

--- getTSrelation.js
+++ getTSrelation.js
@@ -7,36 +7,50 @@ var fs = require('fs'),
 
 //constant
 var START_YEAR = 75,
-    END_YEAR = 102;
+    END_YEAR = 76;
 
 var resultArray = []; //{TID,TName,SName}
 
-// var sql2 = format("SELECT PersonId, Name FROM person LIMIT 0,30");
-// connection.query(sql2, function(err, teachers) {
-//     if (err) return;
-//     async.each(teachers, function(teacher, callback) {}, function(err) {});
-// });
-var yr = START_YEAR;
-var name = '陳澤生';
-async.whilst(
-    function() {
-        return yr < END_YEAR;
-    },
-    function(callback) {
-        var command = format("'http://ndltd.ncl.edu.tw/cgi-bin/gs32/gsweb.cgi/ccd=FZUhN_/search' -H 'Cookie: ccd=pW7aOE; BIGipServerpool_query=426092736.20480.0000; style=ncl' -H 'Origin: http://ndltd.ncl.edu.tw' -H 'Accept-Encoding: gzip,deflate,sdch' -H 'Accept-Language: zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.146 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Cache-Control: max-age=0' -H 'Referer: http://ndltd.ncl.edu.tw/cgi-bin/gs32/gsweb.cgi/ccd=FZUhN_/search?mode=cmd' -H 'Connection: keep-alive' --data 'qs0=%22%s%22.ad+AND+%22%d%22.yr&qf0=_hist_&gs32search.x=27&gs32search.y=6&displayonerecdisable=1&dbcode=nclcdr&action=&op=&h=&histlist=1%2C+2%2C+3&opt=m&_status_=search__v2' --compressed", encodeURIComponent(name), yr);
+var sql2 = format("SELECT PersonId, Name FROM person LIMIT 5476,3");
+connection.query(sql2, function(err, teachers) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    async.each(teachers, searchForCertainTeacher, function(err) {
+        fs.writeFile('temp.txt', JSON.stringify(resultArray));
+        return;
+    });
+});
+
+/*
+ *@param teacher {PersonId,Name}
+ *@param callback is called when this search is over.
+ *	callback(err,results)
+ */
+var searchForCertainTeacher = function(teacher, callback) {
+    var name = teacher.Name,
+        tid = teacher.PersonId;
+    var yr = START_YEAR;
+    async.whilst(
+        function() {
+            return yr < END_YEAR;
+        },
+        function(callback) {
+            var command = format("'http://ndltd.ncl.edu.tw/cgi-bin/gs32/gsweb.cgi/ccd=FZUhN_/search' -H 'Cookie: ccd=pW7aOE; BIGipServerpool_query=426092736.20480.0000; style=ncl' -H 'Origin: http://ndltd.ncl.edu.tw' -H 'Accept-Encoding: gzip,deflate,sdch' -H 'Accept-Language: zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.146 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Cache-Control: max-age=0' -H 'Referer: http://ndltd.ncl.edu.tw/cgi-bin/gs32/gsweb.cgi/ccd=FZUhN_/search?mode=cmd' -H 'Connection: keep-alive' --data 'qs0=%22%s%22.ad+AND+%22%d%22.yr&qf0=_hist_&gs32search.x=27&gs32search.y=6&displayonerecdisable=1&dbcode=nclcdr&action=&op=&h=&histlist=1%2C+2%2C+3&opt=m&_status_=search__v2' --compressed", encodeURIComponent(name), yr);
 
 
 
-        curl.run(command, function(err, result) {
-            if (err) throw err;
-            // console.log(result.stats);
-            // fs.writeFile('file.html', result.payload);
-            $ = cheerio.load(result.payload);
-            var arr = [];
-            $('td.std2').each(function(i, elem) {
-                arr[i] = $(elem).text();
-            });
-            /* Now, array like below: 
+            curl.run(command, function(err, result) {
+                if (err) throw err;
+                // console.log(result.stats);
+                // fs.writeFile('file.html', result.payload);
+                $ = cheerio.load(result.payload);
+                var arr = [];
+                $('td.std2').each(function(i, elem) {
+                    arr[i] = $(elem).text();
+                });
+                /* Now, array like below: 
 				[ '運用HL7介面導入隨身醫囑之研究',
 				  '國立成功大學／工程科學系專班／90／碩士',
 				  '研究生:李賢輝',
@@ -63,23 +77,25 @@ async.whilst(
 				  '指導教授:陳澤生',
 				  '\n\n 電子全文   紙本論文\n\n' ]
 	        */
-            var index = 0;
-            while (index < arr.length) {
-                resultArray.push({
-                    TID: 0,
-                    TName: name,
-                    SName: arr[index + 2].split(':')[1]
-                });
-                index += 5;
-            }
-            yr++;
-            callback(null, format("%s:%d", name, yr));
-        });
+                var index = 0;
+                while (index < arr.length) {
+                    resultArray.push({
+                        TID: tid,
+                        TName: name,
+                        SName: arr[index + 2].split(':')[1]
+                    });
+                    index += 5;
+                }
+                yr++;
+                callback(null, format("%s:%d", name, yr));
+            });
 
-    },
-    function(err) {
-        //this teacher is searched over.
-        fs.writeFile('temp.txt', JSON.stringify(resultArray));
-        console.log("finish");
-    }
-);
+        },
+        function(err) {
+            //this teacher is searched over.
+            // fs.writeFile('temp.txt', JSON.stringify(resultArray));
+            console.log("finish : ", teacher.Name);
+            callback(err, format("%s finish", teacher.Name));
+        }
+    );
+}
